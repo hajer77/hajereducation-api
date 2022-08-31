d24180475035bcd1f8895fff248c568cad152504
Hajer Dahech <hajeriefst@gmail.com>
2022-08-31 21:39:49 +0100

    index

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const articles = [];
 
 const hajereducationCategories=[
   {
+    category:'python',
+    address:'https://hajereducation.tn/category/python-3-7/'
+  },
+  {
+    category:'netbeans',
+    address:'https://hajereducation.tn/category/netbeans-tutorial/'
+  },
+  {
+    category:'matlab',
+    address:'https://hajereducation.tn/category/matlab/'
+  },
+  {
+    category:'embeddedSystem',
+    address:'https://hajereducation.tn/category/embedded-system-projects/'
+  },
+  {
     category:'chimie',
     address:'https://hajereducation.tn/category/chimie/'
   },
@@ -22,7 +38,28 @@ const hajereducationCategories=[
   {
      category:'coding',
       address:'https://hajereducation.tn/category/coding/'
-  }
+  },
+  {
+    category:'bac',
+    address:'https://hajereducation.tn/category/bac/'
+  },
+  {
+    category:'1ereAnnee',
+    address:'https://hajereducation.tn/category/1ere-annee-%d8%a3%d9%88%d9%84%d9%89-%d8%ab%d8%a7%d9%86%d9%88%d9%8a/'
+  },
+  {
+    category:'2emeAnnee',
+    address:'https://hajereducation.tn/category/2eme-annee-secondaire/'
+  },
+  {
+   category:'informatique',
+   address:'https://hajereducation.tn/category/informatique/'
+  },
+  {
+    category:'manuelScolaire',
+    address:'https://hajereducation.tn/category/manuel-scolaire/'
+   }
+   
 ];
 
 
